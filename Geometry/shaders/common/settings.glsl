f2deeb441df22f1d917a0e70134a352aed8946d3
shBLOCK <shblock3@gmail.com>
2025-04-01 21:35:24 +0800

    Add comment in settings.glsl; change some value names in lang file

--- Geometry/shaders/common/settings.glsl
+++ Geometry/shaders/common/settings.glsl
@@ -1,6 +1,8 @@
 #define FRAME_THICKNESS 0.03 // [0.0 0.01 0.03 0.05 0.1 0.15 0.2 0.3 0.4 0.5]
 
+// 0: triangle; 1: quad
 #define GEOMETRY_MODE 0 // [0 1]
+// 0: per vertex; 1: primitive center; 2: min vertex
 #define DISTANCE_MODE 1 // [0 1 2]
 
 #define FACE_FADE_OFFSET 5.0 // [-INF -50.0 -30.0 -20.0 -15.0 -10.0 -7.0 -5.0 -3.0 -2.0 -1.0 0.0 1.0 2.0 3.0 5.0 7.0 10.0 15.0 20.0 30.0 50.0 INF] 
